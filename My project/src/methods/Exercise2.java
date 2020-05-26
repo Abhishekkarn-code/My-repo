4afe35a33ece899330ee10521b9cb1216bef99fd
Abhishekkarn-code <proabhishek732@gmail.com>
2020-05-26 21:52:25 +0545

    modified

--- My project/src/methods/Exercise2.java
+++ My project/src/methods/Exercise2.java
@@ -8,9 +8,7 @@ public class Exercise2 {
 		try(Scanner sc = new Scanner(System.in))
 		{
 			System.out.println("Enter three value");
-			int a= sc.nextInt();
-			int b= sc.nextInt();
-			int c= sc.nextInt();
+			
 			
 			float result = getAverage(a,b,c);
 			System.out.println(result+" is average of three values");
